bd46eb9d0ad8ce5b07c55a47bbc22fea39949dd1
S KEERTHI <keerthishivapur3@gmail.com>
2025-09-25 16:56:37 +0530

    Add files via upload

---
+++ WineQualityPrediction/datascience/pipeline/model_trainer_pipeline.py
@@ -0,0 +1,16 @@
+from datascience.config.configuration import ConfigurationManager
+from datascience.components.model_trainer import ModelTrainer
+from datascience import logger
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_training(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
